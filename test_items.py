1a9a58cbd97261f4918d3e1bfdee94b0258a16db
Zerrau <mogilinsa@mail.ru>
2020-02-27 09:50:55 +0300

    add assert

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 import time
 import unittest
-import pytest
+
+
+# import pytest
 
 
 class SubclassesTest(unittest.TestCase):
@@ -10,8 +12,15 @@ class SubclassesTest(unittest.TestCase):
             link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/'
             browser = webdriver.Chrome()
             browser.get(link)
+            browser.find_element_by_xpath('//*[@id="add_to_basket_form"]/button').click()
+            browser.find_element_by_xpath('//*[@id="messages"]/div[3]/div/p[2]/a[1]').click()
+            book = browser.find_element_by_xpath('//*[@id="basket_formset"]/div/div/div[2]/h3/a')  # Coders at Work
+            assert book.text == 'Coders at Work', 'Book title not found'
+            get_value = browser.find_element_by_xpath('//*[@id="id_form-0-quantity"]').get_attribute("value")  # value 1
+            assert get_value == '1', 'Wrong amount of books'
+            price = browser.find_element_by_xpath('//*[@id="basket_formset"]/div/div/div[4]/p')  # price £19.99
+            assert price.text == '£19.99', 'Wrong book price'
         finally:
-            time.sleep(3)
             browser.quit()
 
 
